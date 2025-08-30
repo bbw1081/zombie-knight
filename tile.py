37b007555b94c512bc44dd6734f95cb515e8e120
Richard Bradley Wilkinson <bbw1081@g.rit.edu>
2025-08-30 11:42:34 -0400

    v0.4.0 --Added functionality to tile.py --Added function to generate tiles from the tile map in zombie_knight.py --Added functionality to ruby_maker.py --Updated the ruby maker in zombie_knight.py to allow for animation to run

--- tile.py
+++ tile.py
@@ -1,8 +1,30 @@
-import pygame, random
+import pygame
 
 class Tile(pygame.sprite.Sprite):
     """A class to represent a tile in the display"""
 
-    def __init__(self):
+    def __init__(self, x, y, image_int, main_group, sub_group=None):
         """create the tile"""
-        pass
+        super().__init__()
+        #load in image and add it to the subgroup
+        if image_int == 1: #dirt image
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (1).png"), (32, 32))
+        elif image_int == 2: #ground platform
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (2).png"), (32, 32))
+            sub_group.add(self)
+        elif image_int == 3: #left platform
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (3).png"), (32, 32))
+            sub_group.add(self)
+        elif image_int == 4: #middle platform
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (4).png"), (32, 32))
+            sub_group.add(self)
+        elif image_int == 5: #right platform
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (5).png"), (32, 32))
+            sub_group.add(self)
+
+        #add every tile to main group
+        main_group.add(self)
+
+        #get the rect and position in the display surface
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
--- zombie_knight.py
+++ zombie_knight.py
@@ -45,7 +45,7 @@ tile_map = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -67,8 +67,41 @@ tile_map = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+#generate tile objects from tile map
+#loop through the 23 rows in the map, i moves us down the map
+for i in range(len(tile_map)):
+    #loop through the 40 columns in a given row, j moves us across the map
+    for j in range(len(tile_map[i])):
+        #dirt tile
+        if tile_map[i][j] == 1:
+            Tile(j*32, i*32, 1, main_tile_group)
+        #ground platform tile
+        elif tile_map[i][j] == 2:
+            Tile(j*32, i*32, 2, main_tile_group, platform_group)
+        #left platform tile
+        elif tile_map[i][j] == 3:
+            Tile(j*32, i*32, 3, main_tile_group, platform_group)
+        #middle platform tile
+        elif tile_map[i][j] == 4:
+            Tile(j*32, i*32, 4, main_tile_group, platform_group)
+        #right platform tile
+        elif tile_map[i][j] == 5:
+            Tile(j*32, i*32, 5, main_tile_group, platform_group)
+        #ruby maker
+        elif tile_map[i][j] == 6:
+            RubyMaker(j*32, i*32, main_tile_group)
+        #portals
+        elif tile_map[i][j] == 7:
+            pass
+        elif tile_map[i][j] == 8:
+            pass
+        #player
+        elif tile_map[i][j] == 9:
+            pass
+
+
 #Load in background image
-background_image = pygame.transform.scale(pygame.image.load("../10_1_zombie_knight/assets/images/background.png"),
+background_image = pygame.transform.scale(pygame.image.load("assets/images/background.png"),
                                           (WINDOW_WIDTH, WINDOW_HEIGHT))
 background_rect = background_image.get_rect()
 background_rect.topleft = (0,0)
@@ -83,6 +116,10 @@ while running:
 
     #blit the background
     DISPLAY_SURFACE.blit(background_image, background_rect)
+
+    #draw our tiles
+    main_tile_group.draw(DISPLAY_SURFACE)
+    main_tile_group.update()
 
     #update display and tick clock
     pygame.display.update()
