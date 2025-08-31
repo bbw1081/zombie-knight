21b1ca3b32ae8b9338f7b7c0ef8243031346a1e7
Richard Bradley Wilkinson <bbw1081@g.rit.edu>
2025-08-30 23:36:58 -0400

    v0.5.0 --Implemented the Portal class in the portal.py function --Added portal functionality to zombie_knight.py --Implemented the HUD elements in the Game class --Moved the Game clss from game.py to zombie_knight.py due to an error with the two files attempting to import each other --Added HUD functionality so it now shows on screen

--- zombie_knight.py
+++ zombie_knight.py
@@ -1,5 +1,4 @@
 import pygame, random
-from game import Game
 from tile import Tile
 from player import Player
 from bullet import Bullet
@@ -26,6 +25,96 @@ pygame.display.set_caption("Zombie Knight!")
 FPS = 60
 CLOCK = pygame.time.Clock()
 
+class Game:
+    """A class to help manage gameplay"""
+
+    def __init__(self):
+        """Initialize the game"""
+        #set constant variables
+        self.STARTING_ROUND_TIME = 30
+
+        #set game values
+        self.score = 0
+        self.round_number = 1
+        self.frame_count = 0
+        self.round_time = self.STARTING_ROUND_TIME
+
+        #load in fonts
+        self.title_font = pygame.font.Font("assets/fonts/Poultrygeist.ttf", 48)
+        self.HUD_font = pygame.font.Font("assets/fonts/Pixel.ttf", 24)
+
+    def update(self):
+        """update the game"""
+        #update the round time every second
+        self.frame_count += 1
+        if self.frame_count % FPS == 0:
+            self.round_time -= 1
+            self.frame_count = 0
+
+    def draw(self):
+        """draw the game HUD"""
+        #define colors
+        WHITE = (255, 255, 255)
+        GREEN = (25, 200, 25)
+
+        #set text
+        score_text = self.HUD_font.render("Score: " + str(self.score), True, WHITE)
+        score_rect = score_text.get_rect()
+        score_rect.topleft = (10, WINDOW_HEIGHT - 50)
+
+        health_text = self.HUD_font.render("Health: " + str(100), True, WHITE)
+        health_rect = health_text.get_rect()
+        health_rect.topleft = (10, WINDOW_HEIGHT - 25)
+
+        title_text = self.title_font.render("Zombie Knight", True, GREEN)
+        title_rect = title_text.get_rect()
+        title_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 25)
+
+        round_text = self.HUD_font.render("Night: " + str(self.round_number), True, WHITE)
+        round_rect = round_text.get_rect()
+        round_rect.topright = (WINDOW_WIDTH - 10, WINDOW_HEIGHT - 50)
+
+        time_text = self.HUD_font.render("Sunrise in: " + str(self.round_time), True, WHITE)
+        time_rect = time_text.get_rect()
+        time_rect.topright = (WINDOW_WIDTH - 10, WINDOW_HEIGHT - 25)
+
+        #draw the HUD
+        DISPLAY_SURFACE.blit(score_text, score_rect)
+        DISPLAY_SURFACE.blit(health_text, health_rect)
+        DISPLAY_SURFACE.blit(title_text, title_rect)
+        DISPLAY_SURFACE.blit(round_text, round_rect)
+        DISPLAY_SURFACE.blit(time_text, time_rect)
+
+
+    def add_zombie(self):
+        """add a zombie to the game"""
+        pass
+
+    def check_collisions(self):
+        """Check collisions that affect gameplay"""
+        pass
+
+    def check_round_completion(self):
+        """Check if the player survived a night"""
+        pass
+
+    def check_game_over(self):
+        """Check to see if the player lost the game"""
+        pass
+
+    def start_new_round(self):
+        """start a new night"""
+        pass
+
+    def pause_game(self):
+        """pause the game"""
+        pass
+
+    def reset_game(self):
+        """reset the game"""
+        pass
+
+
 #Create Sprite Groups
 main_tile_group = pygame.sprite.Group()
 platform_group = pygame.sprite.Group()
@@ -39,12 +128,12 @@ portal_group = pygame.sprite.Group()
 ruby_group = pygame.sprite.Group()
 
 #Create the tile map
-#0 --> no tile, 1 --> dirt tile, 2-5 --> platforms, 6 --> ruby maker, 7-8 --> platform, 9 --> player
+#0 --> no tile, 1 --> dirt tile, 2-5 --> platforms, 6 --> ruby maker, 7-8 --> portals, 9 --> player
 #23 rows and 40 columns
 tile_map = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0],
 [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -62,7 +151,7 @@ tile_map = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0],
 [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
@@ -92,9 +181,9 @@ for i in range(len(tile_map)):
             RubyMaker(j*32, i*32, main_tile_group)
         #portals
         elif tile_map[i][j] == 7:
-            pass
+            Portal(j*32, i*32, "green", portal_group)
         elif tile_map[i][j] == 8:
-            pass
+            Portal(j*32, i*32, "purple", portal_group)
         #player
         elif tile_map[i][j] == 9:
             pass
@@ -105,6 +194,9 @@ background_image = pygame.transform.scale(pygame.image.load("assets/images/backg
                                           (WINDOW_WIDTH, WINDOW_HEIGHT))
 background_rect = background_image.get_rect()
 background_rect.topleft = (0,0)
+
+#create a game
+my_game = Game()
 
 """MAIN GAME LOOP"""
 running = True
@@ -118,8 +210,16 @@ while running:
     DISPLAY_SURFACE.blit(background_image, background_rect)
 
     #draw our tiles
-    main_tile_group.draw(DISPLAY_SURFACE)
     main_tile_group.update()
+    main_tile_group.draw(DISPLAY_SURFACE)
+
+    #draw and update sprite groups
+    portal_group.update()
+    portal_group.draw(DISPLAY_SURFACE)
+
+    #update and draw game
+    my_game.update()
+    my_game.draw()
 
     #update display and tick clock
     pygame.display.update()
